415e46193425e767761152e09f5765734a8e58c0
dolphingarlic <dolphingarlic@gmail.com>
2020-05-09 09:37:23 +0200

    Styling

--- bot.py
+++ bot.py
@@ -15,14 +15,16 @@ import discord
 client = discord.Client()
 
 pattern = re.compile(
-    r'https:\/\/github\.com\/(?P<repo>.+)\/blob\/(?P<branch>.+?)\/(?P<file_path>.+?)(?P<extension>\.(?P<language>.+))*#L(?P<start_line>[0-9]+)(-L(?P<end_line>[0-9]+))*'
+    r'https:\/\/github\.com\/(?P<repo>.+)\/blob\/(?P<branch>.+?)\/(?P<file_path>.+?)' +
+    r'(?P<extension>\.(?P<language>.+))*#L(?P<start_line>[0-9]+)(-L(?P<end_line>[0-9]+))*'
 )
 
 
 @client.event
 async def on_message(message):
     '''
-    Checks if the message starts is a GitHub snippet, then removes the embed, then sends the snippet in Discord
+    Checks if the message starts is a GitHub snippet, then removes the embed,
+    then sends the snippet in Discord
     '''
 
     match = pattern.search(message.content)
@@ -30,7 +32,7 @@ async def on_message(message):
         d = match.groupdict()
         response_json = requests.get(
             f'https://api.github.com/repos/{d["repo"]}/contents/{d["file_path"]}' +
-            f'{d["extension"] if d["extension"] else None}?ref={d["branch"]}'
+            f'{d["extension"] if d["extension"] else ""}?ref={d["branch"]}'
         ).json()
         file_contents = base64.b64decode(
             response_json['content']).decode('utf-8')
@@ -45,18 +47,24 @@ async def on_message(message):
 
         required = split_file_contents[start_line - 1:end_line]
 
-        while all(line.startswith('\t') for line in required) or all(line.startswith(' ') for line in required):
+        while all(line.startswith('\t') or line.startswith(' ') for line in required):
             required = list(map(lambda line: line[1:], required))
 
-        required = '\n'.join(required).rstrip().replace('`', '\`')
+        required = '\n'.join(required).rstrip().replace('`', r'\`')
 
         await message.edit(suppress=True)
-        if (len(required) != 0):
+        if len(required) != 0:
             await message.channel.send(f'```{d["language"]}\n{required}```')
+        else:
+            await message.channel.send('``` ```')
 
 
 @client.event
 async def on_ready():
+    '''
+    Just prints when the bot is ready
+    '''
+
     print(f'{client.user} has connected to Discord!')
 
 client.run(os.environ['DISCORD_TOKEN'])
