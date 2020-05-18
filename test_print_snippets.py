7198a76e705582005a4240172c3d9f3c3572468b
dolphingarlic <dolphingarlic@gmail.com>
2020-05-18 15:43:30 +0200

    Improved tests

--- test_print_snippets.py
+++ test_print_snippets.py
@@ -24,16 +24,15 @@ async def test_github():
 
     dpytest.configure(bot)
 
+    # Test single line
     await dpytest.message('https://github.com/dolphingarlic/git-the-lines/blob/master/README.md#L1')
     dpytest.verify_message('```md\n# Git the lines```')
 
+    # Test no file extension
     await dpytest.message('https://github.com/dolphingarlic/git-the-lines/blob/master/Procfile#L1')
     dpytest.verify_message('```Procfile\nworker: python bot.py```')
-    await dpytest.message('https://github.com/dolphingarlic/git-the-lines/blob/master/Procfile#L1-L1000')
-    dpytest.verify_message('```Procfile\nworker: python bot.py```')
-    await dpytest.message('https://github.com/dolphingarlic/git-the-lines/blob/master/Procfile#L1-L0')
-    dpytest.verify_message('```Procfile\nworker: python bot.py```')
 
+    # Test indexing of multi-line snippets
     await dpytest.message('https://github.com/dolphingarlic/git-the-lines/blob/master/bot.py#L1-L2')
     dpytest.verify_message('```py\n"""\nGit the lines```')
     await dpytest.message('https://github.com/dolphingarlic/git-the-lines/blob/master/bot.py#L2-L1')
@@ -58,13 +57,20 @@ async def test_gitlab():
 
     dpytest.configure(bot)
 
+    # Test multi-line snippet
     await dpytest.message('https://gitlab.com/dolphingarlic/bot-testing/-/blob/master/example.test.py#L1-2')
     dpytest.verify_message('```py\nprint(help(str))\nprint(\'Hi\')```')
 
+    # Test nested file
     await dpytest.message('https://gitlab.com/dolphingarlic/bot-testing/-/blob/master/nested/file.py#L1-2')
     dpytest.verify_message('```py\nprint(\'Hey there!\')\nprint(\'Nice to see you\')```')
     await dpytest.message('https://gitlab.com/dolphingarlic/bot-testing/-/blob/master/nested/file.py#L1')
     dpytest.verify_message('```py\nprint(\'Hey there!\')```')
 
+    # Test weird file path
     await dpytest.message('https://gitlab.com/dolphingarlic/bot-testing/-/blob/master/nested/fi.l/e.py#L2')
     dpytest.verify_message('```py\nprint(1 + 3)```')
+
+    # Test no file extension
+    await dpytest.message('https://gitlab.com/dolphingarlic/bot-testing/-/blob/master/nested/fi.l/ee#L1')
+    dpytest.verify_message('```ee\nMinu nimi on Andi```')
