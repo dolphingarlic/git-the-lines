b21c00b3b2ca8d431fb6fd0898be5e0a4fbed7e0
Mukundan Senthil <mukundan314@gmail.com>
2020-06-30 21:03:30 +0530

    Add repo_widgets for gitlab

--- cogs/repo_widgets.py
+++ cogs/repo_widgets.py
@@ -11,11 +11,13 @@ import re
 import discord
 from discord.ext.commands import Cog
 
-from cogs.utils import fetch_http
+from cogs.utils import fetch_http, orig_to_encode
 
 
 GITHUB_RE = re.compile(
     r'https://github\.com/(?P<owner>[^/]+?)/(?P<repo>[^/]+?)(?:\s|$)')
+GITLAB_RE = re.compile(
+    r'https://gitlab\.com/(?P<owner>[^/]+?)/(?P<repo>[^/]+?)(?:\s|$)')
 
 
 class RepoWidgets(Cog):
@@ -33,8 +35,9 @@ class RepoWidgets(Cog):
         """
 
         gh_match = GITHUB_RE.search(message.content)
+        gl_match = GITLAB_RE.search(message.content)
 
-        if gh_match and not message.author.bot:
+        if (gh_match or gl_match) and not message.author.bot:
             for gh in GITHUB_RE.finditer(message.content):
                 d = gh.groupdict()
                 headers = {}
@@ -61,6 +64,35 @@ class RepoWidgets(Cog):
                 ).set_thumbnail(url=repo['owner']['avatar_url'])
                 if repo['homepage']:
                     embed.add_field(name='Website', value=repo['homepage'])
+                await message.channel.send(embed=embed)
+
+            for gl in GITLAB_RE.finditer(message.content):
+                d = gl.groupdict()
+                await orig_to_encode(d)
+                headers = {}
+                if 'GITLAB_TOKEN' in os.environ:
+                    headers['PRIVATE-TOKEN'] = os.environ["GITLAB_TOKEN"]
+                repo = await fetch_http(
+                    self.session,
+                    f'https://gitlab.com/api/v4/projects/{d["owner"]}%2F{d["repo"]}',
+                    'json',
+                    headers=headers,
+                )
+
+                embed = discord.Embed(
+                    title=repo['path_with_namespace'],
+                    description='No description provided' if repo[
+                        'description'] == "" else repo['description'],
+                    url=repo['web_url'],
+                    color=0x111111
+                ).set_footer(
+                    text=f'Stars: {repo["star_count"]} | ' +
+                         f'Forks: {repo["forks_count"]}'
+                )
+
+                if repo['avatar_url'] is not None:
+                    embed.set_thumbnail(url=repo['avatar_url'])
+
                 await message.channel.send(embed=embed)
 
             await message.edit(suppress=True)
