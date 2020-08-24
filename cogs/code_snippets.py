b0ac02fa679a9871074b11c5b1f27871a125d993
Mukundan Senthil <mukundan314@gmail.com>
2020-08-24 12:18:35 +0530

    Add support for links without line ranges

--- cogs/code_snippets.py
+++ cogs/code_snippets.py
@@ -14,24 +14,25 @@ from cogs.utils import (fetch_bitbucket_snippet, fetch_github_gist_snippet,
                         fetch_github_snippet, fetch_gitlab_snippet)
 
 GITHUB_RE = re.compile(
-    r'https://github\.com/(?P<repo>\S+?)/blob/(?P<path>\S+/\S+)'
-    r'#L(?P<start_line>\d+)([-~:]L(?P<end_line>\d+))?\b'
+    r'https://github\.com/(?P<repo>\S+?)/blob/(?P<path>\S+/[^\s#]+)'
+    r'(#L(?P<start_line>\d+)([-~:]L(?P<end_line>\d+))?)?($|\s)'
 )
 
 GITHUB_GIST_RE = re.compile(
     r'https://gist\.github\.com/([^/]+)/(?P<gist_id>[^\W_]+)/*'
     r'(?P<revision>[^\W_]*)/*#file-(?P<file_path>\S+?)'
-    r'-L(?P<start_line>\d+)([-~:]L(?P<end_line>\d+))?\b'
+    r'(-L(?P<start_line>\d+)([-~:]L(?P<end_line>\d+))?)?($|\s)'
 )
 
 GITLAB_RE = re.compile(
-    r'https://gitlab\.com/(?P<repo>\S+?)/\-/blob/(?P<path>\S+/\S+)'
-    r'#L(?P<start_line>\d+)([-](?P<end_line>\d+))?\b'
+    r'https://gitlab\.com/(?P<repo>\S+?)/\-/blob/(?P<path>\S+/[^\s#]+)'
+    r'(#L(?P<start_line>\d+)([-](?P<end_line>\d+))?)?($|\s)'
 )
 
 BITBUCKET_RE = re.compile(
-    r'https://bitbucket\.org/(?P<repo>\S+?)/src/(?P<ref>\S+?)/'
-    r'(?P<file_path>\S+?)#lines-(?P<start_line>\d+)(:(?P<end_line>\d+))?\b'
+    r'https://bitbucket\.org/(?P<repo>\S+?)/src/'
+    r'(?P<ref>\S+?)/(?P<file_path>[^\s#]+)'
+    r'(#lines-(?P<start_line>\d+)(:(?P<end_line>\d+))?)?($|\s)'
 )
 
 
