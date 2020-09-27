cbcd3f10f98e037bb97b6de4da69879f027f7a43
Rohini Senthil <rohinivsenthil@gmail.com>
2020-09-27 09:48:24 +0530

    add gitlab support for commits

--- cogs/commit_widgets.py
+++ cogs/commit_widgets.py
@@ -8,6 +8,7 @@ of the first matched snippet url
 import datetime
 import os
 import re
+from urllib.parse import quote_plus
 
 import discord
 from discord.ext.commands import Cog
@@ -17,6 +18,10 @@ from cogs.utils import fetch_http
 
 GITHUB_RE = re.compile(
     r'https://github\.com/(?P<owner>[^/\s]+)/(?P<repo>[^/\s]+)/commit/'
+    r'(?P<commit>[^/\s]+)'
+)
+GITLAB_RE = re.compile(
+    r'https://gitlab\.com/(?P<owner>[^/\s]+)/(?P<repo>[^/\s]+)/-/commit/'
     r'(?P<commit>[^/\s]+)'
 )
 
@@ -36,8 +41,10 @@ class CommitWidgets(Cog):
         """
 
         gh_match = GITHUB_RE.search(message.content)
+        gl_match = GITLAB_RE.search(message.content)
 
-        if gh_match and not message.author.bot:
+        if (gh_match or gl_match) and not message.author.bot:
+
             for gh in GITHUB_RE.finditer(message.content):
                 d = gh.groupdict()
                 headers = {}
@@ -76,6 +83,47 @@ class CommitWidgets(Cog):
                 ).set_footer(
                     text=f'{commit["author"]["login"]} committed',
                     icon_url=commit['author']['avatar_url']
+                )
+
+                await message.channel.send(embed=embed)
+
+            for gl in GITLAB_RE.finditer(message.content):
+                d = gl.groupdict()
+                headers = {}
+                if 'GITLAB_TOKEN' in os.environ:
+                    headers['PRIVATE-TOKEN'] = os.environ["GITLAB_TOKEN"]
+                commit = await fetch_http(
+                    self.session,
+                    f'https://gitlab.com/api/v4/projects/{quote_plus(d["owner"])}%2F{quote_plus(d["repo"])}/repository/commits/{d["commit"]}',
+                    'json',
+                    headers=headers,
+                )
+
+                embed = discord.Embed(
+                    title=f'commit `{commit["id"]}`',
+                    description=commit['message'],
+                    url=commit['web_url'],
+                    timestamp=datetime.datetime.fromisoformat(
+                        commit['authored_date']
+                    ),
+                    color=0x111111
+                ).set_author(
+                    name=f'{d["owner"]}/{d["repo"]}',
+                    url=f'https://gitlab.com/{d["owner"]}/{d["repo"]}'
+                ).add_field(
+                    name="Additions",
+                    value=str(commit['stats']['additions']),
+                    inline=True
+                ).add_field(
+                    name="Deletions",
+                    value=str(commit['stats']['deletions']),
+                    inline=True
+                ).add_field(
+                    name="Status",
+                    value=commit['status'],
+                    inline=True
+                ).set_footer(
+                    text=f'{commit["author_name"]} committed',
                 )
 
                 await message.channel.send(embed=embed)
