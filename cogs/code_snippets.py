f18d01f4c8ae08351f07cc657fc398449854760e
Andi Qu <31325319+dolphingarlic@users.noreply.github.com>
2021-04-02 15:51:46 +0200

    Added Heptapod regex

--- cogs/code_snippets.py
+++ cogs/code_snippets.py
@@ -10,7 +10,8 @@ import re
 from discord.ext.commands import Cog
 
 from cogs.utils import (fetch_bitbucket_snippet, fetch_github_gist_snippet,
-                        fetch_github_snippet, fetch_gitlab_snippet, wait_for_deletion)
+                        fetch_github_snippet, fetch_gitlab_snippet, fetch_heptapod_snippet,
+                        wait_for_deletion)
 
 GITHUB_RE = re.compile(
     r'https://github\.com/(?P<repo>[a-zA-Z0-9-]+/[\w.-]+)/blob/'
@@ -33,12 +34,25 @@ BITBUCKET_RE = re.compile(
     r'/(?P<file_path>[^#>\s]+)(\?[^#>\s]+)?(#lines-(?P<start_line>\d+)(:(?P<end_line>\d+))?)?'
 )
 
+HEPTAPOD_RE = re.compile(
+    r'https://foss\.heptapod\.net/([a-zA-Z0-9-]+/[\w.-]+)/-/blob/'
+    r'([^#>]+)(\?[^#>]+)?(#L(\d+)([-~:](\d+))?)?'
+)
+
 class CodeSnippets(Cog):
     def __init__(self, bot, session):
         """Initializes the cog's bot"""
 
         self.bot = bot
         self.session = session
+        
+        self.pattern_handlers = [
+            (GITHUB_RE, fetch_github_snippet),
+            (GITHUB_GIST_RE, fetch_github_gist_snippet),
+            (GITLAB_RE, fetch_gitlab_snippet),
+            (BITBUCKET_RE, fetch_bitbucket_snippet),
+            (HEPTAPOD_RE, fetch_heptapod_snippet)
+        ]
 
     @Cog.listener()
     async def on_message(self, message):
@@ -46,28 +60,13 @@ class CodeSnippets(Cog):
         Checks if the message starts is a GitHub snippet, then removes the embed,
         then sends the snippet in Discord
         """
-
-        gh_match = GITHUB_RE.search(message.content)
-        gh_gist_match = GITHUB_GIST_RE.search(message.content)
-        gl_match = GITLAB_RE.search(message.content)
-        bb_match = BITBUCKET_RE.search(message.content)
-
-        if (gh_match or gh_gist_match or gl_match or bb_match) and not message.author.bot:
+        
+        if not message.author.bot:
             message_to_send = ''
 
-            for gh in GITHUB_RE.finditer(message.content):
-                message_to_send += await fetch_github_snippet(self.session, **gh.groupdict())
-
-            for gh_gist in GITHUB_GIST_RE.finditer(message.content):
-                message_to_send += await fetch_github_gist_snippet(self.session, **gh_gist.groupdict())
-
-            for gl in GITLAB_RE.finditer(message.content):
-                message_to_send += await fetch_gitlab_snippet(self.session, **gl.groupdict())
-
-            for bb in BITBUCKET_RE.finditer(message.content):
-                message_to_send += await fetch_bitbucket_snippet(self.session, **bb.groupdict())
-
-            message_to_send = message_to_send[:-1]
+            for pattern, handler in self.pattern_handlers:
+                for match in pattern.finditer(message.content):
+                    message_to_send += await handler(self.session, **match.groupdict())
 
             if 0 < len(message_to_send) <= 2000 and message_to_send.count('\n') <= 50:
                 await wait_for_deletion(message, self.bot, message_to_send)
